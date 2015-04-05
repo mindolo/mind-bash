1a9bdbba54b9d40f6d4a07d27bb2c6f9017b4a1d
Francesco Pedrini <mind@Keyra.lan>
2015-04-05 04:08:52 +0100

    add colonize.sh

---
+++ colonize.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Test if we have git
+which git &> /dev/null
+if [ $? -ne 0 ]; then
+	echo "Please install git before running this installer."
+	exit 1
+fi
+
+_repourl="https://github.com/andsens/homeshick.git"
+_repodir="$HOME/.homesick/repos/homeshick"
+
+echo "Installing homeshick..."
+git clone "$_repourl" "$_repodir"
+echo "Loading homeshick"
+source "$_repodir"/homeshick.sh
+
+homeshick clone mindolo/mind-bash
+exec bash
